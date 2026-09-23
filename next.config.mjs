9d89342000b03341139e30778c786c4939bdc029
Danang Rahmaddiansyah <danangr9a@gmail.com>
2026-09-23 23:01:04 +0700

    abis benerin config der

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/nama-route-asli', // Ganti dengan path/slug dari tab yang kebuka tadi
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
