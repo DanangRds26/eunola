f072c0607c8dc5df23cd0d72ad24d6f49672125d
Danang Rahmaddiansyah <danangr9a@gmail.com>
2026-09-25 23:18:24 +0700

    update setting config

--- src/themes/lolito/config.ts
+++ src/themes/lolito/config.ts
@@ -159,4 +159,106 @@ export const lolitoConfig: InvitationThemeConfig = {
 
     groomPhoto: "1522673607200-164d1b6ce486",
   },
+          preweddingImages: [
+      {
+        src: "/prewedding/prewedding-1.jpeg",
+        alt: "Prewedding 1",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-2.jpeg",
+        alt: "Prewedding 2",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-3.jpeg",
+        alt: "Prewedding 3",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-4.jpeg",
+        alt: "Prewedding 4",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-5.jpeg",
+        alt: "Prewedding 5",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-6.jpeg",
+        alt: "Prewedding 6",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-7.jpeg",
+        alt: "Prewedding 7",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-8.jpeg",
+        alt: "Prewedding 8",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-9.jpeg",
+        alt: "Prewedding 9",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-10.jpeg",
+        alt: "Prewedding 10",
+        enabled: true,
+      },
+      {
+        src: "/prewedding/prewedding-11.jpeg",
+        alt: "",
+        enabled: false,
+      },
+      {
+        src: "/prewedding/prewedding-12.jpeg",
+        alt: "Prewedding 12",
+        enabled: false,
+      },
+      {
+        src: "/prewedding/prewedding-13.jpeg",
+        alt: "Prewedding 13",
+        enabled: false,
+      },
+      {
+        src: "/prewedding/prewedding-14.jpeg",
+        alt: "Prewedding 14",
+        enabled: false, 
+      },
+      {
+        src: "/prewedding/prewedding-15.jpeg",
+        alt: "Prewedding 15",
+        enabled: false, 
+      },
+      {
+        src: "/prewedding/prewedding-16.jpeg",
+        alt: "Prewedding 16",
+        enabled: false, 
+      },
+      {
+        src: "/prewedding/prewedding-17.jpeg",
+        alt: "Prewedding 17",
+        enabled: false, 
+      },
+      {
+        src: "/prewedding/prewedding-18.jpeg",
+        alt: "Prewedding 18",
+        enabled: false, 
+      },
+      {
+        src: "/prewedding/prewedding-19.jpeg",
+        alt: "Prewedding 19",
+        enabled: false, 
+      },
+      {
+        src: "/prewedding/prewedding-20.jpeg",
+        alt: "Prewedding 20",
+        enabled: false, 
+      },
+    ],
 };
